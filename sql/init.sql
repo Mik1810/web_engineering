bcfacb4797f6ca9eb38bc879120ac7416750db62
Mik1810 <michael.piccirilli@student.univaq.it>
2024-04-30 15:53:46 +0200

    Added sql script to create tables, began the population, updated diagrams and model

---
+++ sql/init.sql
@@ -0,0 +1,192 @@
+CREATE DATABASE IF NOT EXISTS webmarket;
+
+USE webmarket;
+
+/*
+Categoria Padre: (ID, Nome)
+Categoria Figlio: (ID, Nome, ID categoria padre)
+Categoria Nipote: (ID, Nome, ID categoria figlio)
+Caratteristica: (ID, Nome, Unit`a di misura, ID categoria nipote)
+Ufficio: (ID, sede, numero, piano, telefono)
+Ordinante: (ID, email, password, ID ufficio)
+Richiesta di Acquisto: (ID, Codice richiesta, Note, Data, Ora, ID ordinante)
+composta: (ID, Valore, ID richiesta di acquisto, ID caratteristica)
+Tecnico dei Preventivi: (ID, email, password)
+Richiesta Presa in Carico: (ID, ID richiesta di acquisto, ID tecnico dei preventivi)
+Proposta: (ID, Codice prodotto, Produttore, Note, Prezzo, Nome prodotto, URL, Stato*, Motiva-
+zione, ID tecnico dei preventivi)
+Amministratore: (ID, email, password)
+Tecnico degli ordini: (ID, email, password)
+Feedback: (ID, nome)
+Ordine di Acquisto: (ID, Stato consegna**, ID feedback ID tecnico degli ordini, ID proposta)
+chiude: (ID, ID ordine, ID ordinante
+* Stato `e una ENUM(”In Attesa”, ”Accettata”, ”Rifiutata”)
+** Stato consegna `e una ENUM(”Presa in carico”,”In consegna”, ”Consegnato”)
+*/
+    DROP TABLE IF EXISTS chiude;
+    DROP TABLE IF EXISTS OrdineAcquisto;
+    DROP TABLE IF EXISTS Feedback;
+    DROP TABLE IF EXISTS TecnicoOrdini;
+    DROP TABLE IF EXISTS Amministratore;
+    DROP TABLE IF EXISTS Proposta;
+    DROP TABLE IF EXISTS RichiestaPresaInCarico;
+    DROP TABLE IF EXISTS TecnicoPreventivi;
+    DROP TABLE IF EXISTS composta;
+    DROP TABLE IF EXISTS RichiestaAcquisto;
+    DROP TABLE IF EXISTS Ordinante;
+    DROP TABLE IF EXISTS Ufficio;
+    DROP TABLE IF EXISTS Caratteristica;
+    DROP TABLE IF EXISTS CategoriaNipote;
+    DROP TABLE IF EXISTS CategoriaFiglio;
+    DROP TABLE IF EXISTS CategoriaPadre;
+
+    CREATE TABLE CategoriaPadre (
+        ID INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+        nome VARCHAR(255) NOT NULL
+    );
+
+    CREATE TABLE CategoriaFiglio (
+        ID INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+        nome VARCHAR(255) NOT NULL,
+        ID_categoria_padre INT UNSIGNED NOT NULL,
+        FOREIGN KEY (ID_categoria_padre) REFERENCES CategoriaPadre(ID)
+        ON DELETE CASCADE ON UPDATE CASCADE
+    );
+
+    CREATE TABLE CategoriaNipote (
+        ID INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+        nome VARCHAR(255) NOT NULL,
+        ID_categoria_figlio INT UNSIGNED NOT NULL,
+        FOREIGN KEY (ID_categoria_figlio) REFERENCES CategoriaFiglio(ID)
+        ON DELETE CASCADE ON UPDATE CASCADE
+
+    );
+
+    CREATE TABLE Caratteristica (
+        ID INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+        nome VARCHAR(255) NOT NULL,
+        unita_di_misura VARCHAR(255) NOT NULL,
+        ID_categoria_nipote INT UNSIGNED NOT NULL,
+        FOREIGN KEY (ID_categoria_nipote) REFERENCES CategoriaNipote(ID)
+        ON DELETE CASCADE ON UPDATE CASCADE
+        /* Per RAM, dato che può essere associato sia a PC Desktop che a Notebook, queste costituiranno due
+           entry in caratteristica del tipo:
+           (1, RAM, GB, ID_notebook)
+           (2, RAM, GB, ID_pc_desktop)
+        */
+    );
+
+    CREATE TABLE Ufficio (
+        ID INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+        sede VARCHAR(255) NOT NULL,
+        numero INT NOT NULL,
+        piano INT NOT NULL,
+        telefono VARCHAR(255) NOT NULL
+    );
+
+    CREATE TABLE Ordinante (
+        ID INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        password VARCHAR(255) NOT NULL,
+        ID_ufficio INT UNSIGNED NOT NULL,
+        FOREIGN KEY (ID_ufficio) REFERENCES Ufficio(ID)
+        ON DELETE CASCADE ON UPDATE CASCADE
+    );
+
+     CREATE TABLE RichiestaAcquisto (
+        ID INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+        codice_richiesta VARCHAR(10) NOT NULL UNIQUE,
+        note TEXT NULL,
+        data TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        ID_ordinante INT UNSIGNED NOT NULL,
+        FOREIGN KEY (ID_ordinante) REFERENCES Ordinante(ID)
+        ON DELETE CASCADE ON UPDATE CASCADE
+    );
+
+    CREATE TABLE composta (
+        ID INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+        valore VARCHAR(255) NOT NULL,
+        /* non per forza il valore è un intero, magari sto ordinando
+           qualcosa il cui valore è una stringa */
+        ID_richiesta_acquisto INT UNSIGNED NOT NULL,
+        ID_caratteristica INT UNSIGNED NOT NULL,
+        FOREIGN KEY (ID_richiesta_acquisto) REFERENCES RichiestaAcquisto(ID)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+        FOREIGN KEY (ID_caratteristica) REFERENCES Caratteristica(ID)
+        ON DELETE CASCADE ON UPDATE CASCADE
+    );
+
+    CREATE TABLE TecnicoPreventivi (
+        ID INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        password VARCHAR(255) NOT NULL
+    );
+
+    CREATE TABLE RichiestaPresaInCarico (
+        ID INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+        ID_richiesta_acquisto INT UNSIGNED NOT NULL,
+        ID_tecnico_preventivi INT UNSIGNED NOT NULL,
+        FOREIGN KEY (ID_richiesta_acquisto) REFERENCES RichiestaAcquisto(ID)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+        FOREIGN KEY (ID_tecnico_preventivi) REFERENCES TecnicoPreventivi(ID)
+        ON DELETE RESTRICT ON UPDATE CASCADE
+    );
+
+    CREATE TABLE Proposta (
+        ID INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+        codice_prodotto VARCHAR(255) NOT NULL UNIQUE,
+        produttore VARCHAR(255) NOT NULL,
+        note TEXT NULL,
+        prezzo DECIMAL(10,2) NOT NULL,
+        nome_prodotto VARCHAR(255) NOT NULL,
+        URL VARCHAR(2048) NOT NULL,
+        stato ENUM('In Attesa', 'Accettata', 'Rifiutata') NOT NULL,
+        motivazione TEXT NULL,
+        ID_tecnico_preventivi INT UNSIGNED NOT NULL,
+        ID_richiesta_presa_in_carico INT UNSIGNED NOT NULL,
+        FOREIGN KEY (ID_richiesta_presa_in_carico) REFERENCES RichiestaPresaInCarico(ID)
+        ON DELETE CASCADE ON UPDATE CASCADE
+    );
+
+    CREATE TABLE Amministratore (
+        ID INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        password VARCHAR(255) NOT NULL
+    );
+
+    CREATE TABLE TecnicoOrdini (
+        ID INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        password VARCHAR(255) NOT NULL
+    );
+
+    CREATE TABLE Feedback (
+        /* Default value: 1 => Feedback non disponibile */
+        ID INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+        nome VARCHAR(255) NOT NULL
+    );
+
+    CREATE TABLE OrdineAcquisto (
+        ID INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+        stato_consegna ENUM('Presa in carico', 'In consegna', 'Consegnato') NOT NULL,
+        ID_feedback INT UNSIGNED NOT NULL DEFAULT 1,
+        /* Default value: 1 => Feedback non disponibile */
+        ID_tecnico_ordini INT UNSIGNED NULL,
+        ID_proposta INT UNSIGNED NOT NULL,
+        FOREIGN KEY (ID_tecnico_ordini) REFERENCES TecnicoOrdini(ID)
+        ON DELETE SET NULL ON UPDATE CASCADE,
+        FOREIGN KEY (ID_proposta) REFERENCES Proposta(ID)
+        ON DELETE RESTRICT ON UPDATE CASCADE,
+        FOREIGN KEY (ID_feedback) REFERENCES Feedback(ID)
+        ON DELETE SET DEFAULT ON UPDATE CASCADE
+    );
+
+    CREATE TABLE chiude (
+        ID INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+        ID_ordine INT UNSIGNED NOT NULL,
+        ID_ordinante INT UNSIGNED NOT NULL,
+        FOREIGN KEY (ID_ordine) REFERENCES OrdineAcquisto(ID)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+        FOREIGN KEY (ID_ordinante) REFERENCES Ordinante(ID)
+        ON DELETE CASCADE ON UPDATE CASCADE
+    );
---
+++ sql/values.sql
@@ -0,0 +1,63 @@
+/* Populate the init.sql tables */
+USE webmarket;
+
+/*
+Categoria Padre: (ID, Nome)
+Categoria Figlio: (ID, Nome, ID categoria padre)
+Categoria Nipote: (ID, Nome, ID categoria figlio)
+Caratteristica: (ID, Nome, Unit`a di misura, ID categoria nipote)
+Ufficio: (ID, sede, numero, piano, telefono)
+Ordinante: (ID, email, password, ID ufficio)
+Richiesta di Acquisto: (ID, Codice richiesta, Note, Data, Ora, ID ordinante)
+composta: (ID, Valore, ID richiesta di acquisto, ID caratteristica)
+Tecnico dei Preventivi: (ID, email, password)
+Richiesta Presa in Carico: (ID, ID richiesta di acquisto, ID tecnico dei preventivi)
+Proposta: (ID, Codice prodotto, Produttore, Note, Prezzo, Nome prodotto, URL, Stato*, Motiva-
+zione, ID tecnico dei preventivi)
+Amministratore: (ID, email, password)
+Tecnico degli ordini: (ID, email, password)
+Feedback: (ID, nome)
+Ordine di Acquisto: (ID, Stato consegna**, ID feedback ID tecnico degli ordini, ID proposta)
+chiude: (ID, ID ordine, ID ordinante
+* Stato `e una ENUM(”In Attesa”, ”Accettata”, ”Rifiutata”)
+** Stato consegna `e una ENUM(”Presa in carico”,”In consegna”, ”Consegnato”)
+*/
+
+-- Popolamento CategoriaPadre
+INSERT INTO CategoriaPadre (nome) VALUES ('Modulistica');
+INSERT INTO CategoriaPadre (nome) VALUES ('Informatica');
+INSERT INTO CategoriaPadre (nome) VALUES ('Mobilio');
+
+-- Popolamento CategoriaFiglio
+INSERT INTO CategoriaFiglio (nome, ID_categoria_padre) VALUES ('Computer', 2);
+INSERT INTO CategoriaFiglio (nome, ID_categoria_padre) VALUES ('Accessori', 2);
+INSERT INTO CategoriaFiglio (nome, ID_categoria_padre) VALUES ('Mobili', 3);
+
+-- Popolamento CategoriaNipote
+INSERT INTO CategoriaNipote (nome, ID_categoria_figlio) VALUES ('PC Desktop', 1);
+INSERT INTO CategoriaNipote (nome, ID_categoria_figlio) VALUES ('Notebook', 1);
+INSERT INTO CategoriaNipote (nome, ID_categoria_figlio) VALUES ('Scrivania', 3);
+
+-- Popolamento Caratteristica
+INSERT INTO caratteristica (nome, unita_di_misura, ID_categoria_nipote) VALUES ('RAM', 'GB', 1);
+INSERT INTO caratteristica (nome, unita_di_misura, ID_categoria_nipote) VALUES ('RAM', 'GB', 2);
+INSERT INTO caratteristica (nome, unita_di_misura, ID_categoria_nipote) VALUES ('CPU', 'GHz', 1);
+INSERT INTO caratteristica (nome, unita_di_misura, ID_categoria_nipote) VALUES ('CPU', 'GHz', 2);
+INSERT INTO caratteristica (nome, unita_di_misura, ID_categoria_nipote) VALUES ('Lunghezza', 'cm', 3);
+
+-- Popolamento Ufficio
+INSERT INTO Ufficio (sede, numero, piano, telefono) VALUES ('Via Roma', 12, 1, '0746229912');
+INSERT INTO Ufficio (sede, numero, piano, telefono) VALUES ('Via Roma', 23, 2, '0746758396');
+INSERT INTO Ufficio (sede, numero, piano, telefono) VALUES ('Via Castello', 11, 1, '0861997534');
+
+-- Popolamento Ordinante
+INSERT INTO Ordinante (email, password, ID_ufficio) VALUES ('michaelpiccirilli3@gmail.com', 'supersafepassword', 3);
+INSERT INTO Ordinante (email, password, ID_ufficio) VALUES ('paoloccigiacomo@gmail.com', 'marilu1234', 1);
+
+-- Popolamento RichiestaAcquisto
+INSERT INTO RichiestaAcquisto (codice_richiesta, note, ID_ordinante) VALUES ('7592783840', 'Voglio un PC Desktop per poter giocare a WarThunder in ufficio', 2);
+INSERT INTO RichiestaAcquisto (codice_richiesta, note, ID_ordinante) VALUES ('7683845728', 'Voglio una scrivania più larga ', 1);
+
+-- Popolamento composta
+INSERT INTO composta (valore, ID_richiesta_acquisto, ID_caratteristica) VALUES ('32', 1, 1);
+INSERT INTO composta (valore, ID_richiesta_acquisto, ID_caratteristica) VALUES ('100', 2, 5);
