e4e9dbe6de289044e60eef10329d8eca55bfcfeb
Mik1810 <michael.piccirilli@student.univaq.it>
2024-05-03 13:08:07 +0200

    Added session handler and some Della Penna's examples

---
+++ src/main/java/it/univaq/webmarket/model/User.java
@@ -0,0 +1,20 @@
+package it.univaq.webmarket.model;
+
+public class User {
+
+    private String password;
+    private String email;
+
+    public User(String password, String email) {
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
