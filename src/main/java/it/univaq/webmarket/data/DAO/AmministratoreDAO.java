bcadec3879364d651f34df8dedd3e15a34796671
Mik1810 <michael.piccirilli@student.univaq.it>
2024-05-13 21:04:07 +0200

    PLS WORK

--- src/main/java/it/univaq/webmarket/data/DAO/AmministratoreDAO.java
+++ src/main/java/it/univaq/webmarket/data/DAO/AmministratoreDAO.java
@@ -1,4 +1,12 @@
 package it.univaq.webmarket.data.DAO;
 
+import it.univaq.webmarket.data.model.Amministratore;
+import it.univaq.webmarket.framework.data.DataException;
+
 public interface AmministratoreDAO {
+
+    //Fixing giacomix issues
+    Amministratore createAmministratore();
+
+    Amministratore getAmministratoreByEmail(String email) throws DataException;
 }
